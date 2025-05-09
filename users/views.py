b547c44c4a458955897e6bbf26b3c290a68ad22d
SvetlanaKobyakova <kobyakasv@mail.ru>
2025-05-09 20:58:30 +0300

    0905

--- users/views.py
+++ users/views.py
@@ -1,26 +1,26 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
-from .forms import RegistrationForm
+from .forms import RegistrationForm, NewRegistrationForm
 
 
 def register(request):
     # когда отправляем заполненную форму на сервер
     if request.method == 'POST':
         # создаем объект формы с данными из запроса
-        form = RegistrationForm(request.POST)
+        form = NewRegistrationForm(request.POST)
         # если форма валидна
         if form.is_valid():
             # создаем объект пользователя без записи в БД
             new_user = form.save(commit=False)
             # хэшируем пароль при помощи set_password
-            new_user.set_password(form.cleaned_data['password1'])
+            new_user.set_password(form.cleaned_data['password'])
             # сохраняем пользователя в БД
             new_user.save()
             context = {'title': 'Регистрация завершена', 'new_user': new_user}
             return render(request, template_name='users/registration_done.html', context=context)
 
     # если метод GET (страница с пустой формой регистрации)
-    form = RegistrationForm()
+    form = NewRegistrationForm()
     context = {'title': 'Регистрация пользователя', 'register_form': form}
     return render(request, template_name='users/registration.html', context=context)
 
