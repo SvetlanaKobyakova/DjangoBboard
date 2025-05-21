b418b3464be89df86253b46808b1e36986e28114
SvetlanaKobyakova <kobyakasv@mail.ru>
2025-05-21 20:48:16 +0300

    2105

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.contrib.auth import password_validation
+from django.contrib.auth.forms import SetPasswordForm
+from django.core.exceptions import ValidationError
 
 
 class RegistrationForm(UserCreationForm):
@@ -38,3 +41,37 @@ class NewRegistrationForm(forms.ModelForm):
         if cleaned_data['password'] != cleaned_data['password2']:
             raise forms.ValidationError('Пароли не совпадают!')
         return cleaned_data['password2']
+
+
+class CustomPasswordChangeForm(SetPasswordForm):
+    old_password = forms.CharField(
+        label='Старый пароль',
+        strip=False,
+        widget=forms.PasswordInput(
+            attrs={'autocomlete': 'current-password', 'autofocus': True}
+        ),
+    )
+    new_password1 = forms.CharField(
+        label='Новый пароль',
+        strip=False,
+        widget=forms.PasswordInput()
+    )
+    new_password2 = forms.CharField(
+        label='Подтверждение нового пароля',
+        strip=False,
+        widget=forms.PasswordInput()
+    )
+    def clean(self):
+        cleaned_data = super().clean()
+        old_password = cleaned_data('old_password')
+        new_password1 = cleaned_data('new_password1')
+        new_password2 = cleaned_data('new_password2')
+
+        if old_password and new_password1:
+            if old_password == new_password1:
+                raise ValidationError('Новый пароль должен отличаться от старого')
+
+        if new_password1 and new_password2 and new_password1 != new_password2:
+            raise ValidationError('Введенные пароли не совпадают')
+
+        return cleaned_data
